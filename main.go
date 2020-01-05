df64954f4dcefa6fa4518d6b508fead55752a5c1
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 13:27:44 -0500

    Imprime la longitud del array

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	edades := [3]uint8{18, 20, 30}
 
+	fmt.Printf("La longitud del array es: %d\n", len(edades))
+
 	for indice, valor := range edades {
 		fmt.Printf("En el índice %d está el valor %d\n", indice, valor)
 	}
