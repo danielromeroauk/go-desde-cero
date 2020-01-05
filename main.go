8c58d551fa8596ac13aaf4e87ba356bb1a8db104
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 12:18:26 -0500

    Asigna las variables dentro de los paréntesis de var

--- main.go
+++ main.go
@@ -4,12 +4,10 @@ import "fmt"
 
 func main() {
 	var (
-		nombre, apellido string
-		edad             int
+		nombre   = "Daniel"
+		apellido = "Romero"
+		edad     = 28
 	)
-	nombre = "Daniel"
-	apellido = "Romero"
-	edad = 28
 	fmt.Printf("nombre es %T, apellido es %T y edad es %T\n", nombre, apellido, edad)
 	fmt.Printf("nombre=%s\n", nombre)
 	fmt.Printf("apellido=%s\n", apellido)
