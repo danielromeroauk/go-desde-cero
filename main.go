1777ca6ef46a0547898b2a93c67fc0ebba75ec7d
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 14:09:23 -0500

    Imprime la longitud de un string

--- main.go
+++ main.go
@@ -3,11 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	frutas := make(map[string]int, 2)
-	frutas["mango"] = 5
-	frutas["manzana"] = 10
-
-	for llave, valor := range frutas {
-		fmt.Printf("Llave %s: %d\n", llave, valor)
-	}
+	var nombre string
+	nombre = "Daniel"
+	fmt.Println(len(nombre))
 }
