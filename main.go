2de44a69c8f5f995cd505bfa0591f2b70ec892dc
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 14:28:41 -0500

    Imprime el tipo de dato un booleano y usa una expresión de ejemplo

--- main.go
+++ main.go
@@ -5,20 +5,6 @@ import "fmt"
 import "reflect"
 
 func main() {
-	var nombre string
-	// nombre = "Daniel" + " Romero"
-	// nombre = "Daniel" + "\nRomero"
-	// nombre = "Daniel\nRomero"
-	nombre = `Daniel Romero`
-
-	fmt.Println(reflect.TypeOf(nombre[0]))
-
-	// fmt.Println(nombre[0])
-	// fmt.Println(string(nombre[0]))
-
-	for posicion, letra := range nombre {
-		fmt.Printf("Posición %d, letra \"%s\"\n", posicion, string(letra))
-	}
-
-	fmt.Printf("Hay %d letras en total\n", len(nombre))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(5 < 10 && 5 != 50) // true
 }
