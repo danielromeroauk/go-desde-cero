6dec0e7f957623886f14013c36744302e6c785e1
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 13:24:45 -0500

    Usa la forma corta para declarar y asignar valores al array edades

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var edades [3]uint8
-	edades[0] = 18
-	edades[1] = 20
-	edades[2] = 30
+	edades := [3]uint8{18, 20, 30}
 
 	for indice, valor := range edades {
 		fmt.Printf("En el índice %d está el valor %d\n", indice, valor)
