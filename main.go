401e55eea8be15f822b2d4e70055413dc6caed15
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 13:40:16 -0500

    Crea el slice frutas con make

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	frutas := []string{"manzana", "peras"}
+	frutas := make([]string, 2)
+	frutas[0] = "manzana"
+	frutas[1] = "pera"
 
 	fmt.Println(frutas)
 }
