24ca582a20ba560bf0031059fc2faca942a2dbc8
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 14:36:50 -0500

    Crea un ejemplo de if/else

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import "fmt"
 
-import "reflect"
-
 func main() {
-	fmt.Println(reflect.TypeOf(true))
-	fmt.Println(5 < 10 && 5 != 50) // true
+	if edad := 15; edad >= 18 {
+		fmt.Println("Eres mayor de edad en Colombia")
+		fmt.Println(edad)
+	} else {
+		fmt.Println("Eres menor de edad en Colombia")
+		fmt.Println(edad)
+	}
 }
