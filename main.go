406b349711f2f2715b701811da00b64945cadee8
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 13:11:25 -0500

    Suma peras con manzanas

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	nombre, apellido, edad := "Daniel", "Romero", 28
+	var peras int8 = 4
+	var manzanas int8 = 9
 
-	fmt.Printf("nombre es %T, apellido es %T y edad es %T\n", nombre, apellido, edad)
-	fmt.Printf("nombre=%s\n", nombre)
-	fmt.Printf("apellido=%s\n", apellido)
-	fmt.Printf("edad=%d\n", edad)
+	frutas := peras + manzanas
+
+	fmt.Printf("Tengo %v frutas\n", frutas)
 }
