675d3ad3b4b18ca435b216a082246f12ce2166b8
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 13:41:45 -0500

    Usa append para agregar elementos al slice frutas

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	frutas := make([]string, 2)
-	frutas[0] = "manzana"
-	frutas[1] = "pera"
+	frutas := make([]string, 0, 10)
+	frutas = append(frutas, "mango")
 
 	fmt.Println(frutas)
 }
