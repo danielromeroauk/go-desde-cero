f56ed4af1024d1f200a1bab9c16fe2c5ddb7a460
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 13:38:55 -0500

    Crea el slice frutas y lo imprime en pantalla

--- main.go
+++ main.go
@@ -3,11 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	edades := [3]uint8{18, 20, 30}
+	frutas := []string{"manzana", "peras"}
 
-	fmt.Printf("La longitud del array es: %d\n", len(edades))
-
-	for indice, valor := range edades {
-		fmt.Printf("En el índice %d está el valor %d\n", indice, valor)
-	}
+	fmt.Println(frutas)
 }
