776f892cfe8cc23a2711d903c61dd5691429a422
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 14:22:23 -0500

    Muestra las diferentes formas de asignar un string
    Imprime las posiciones de un string.
    
    Imprime el tipo de dato de una letra.
    
    Imprime la cantidad de letras que tiene un string.

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import "fmt"
 
+import "reflect"
+
 func main() {
 	var nombre string
-	nombre = "Daniel"
-	fmt.Println(len(nombre))
+	// nombre = "Daniel" + " Romero"
+	// nombre = "Daniel" + "\nRomero"
+	// nombre = "Daniel\nRomero"
+	nombre = `Daniel Romero`
+
+	fmt.Println(reflect.TypeOf(nombre[0]))
+
+	// fmt.Println(nombre[0])
+	// fmt.Println(string(nombre[0]))
+
+	for posicion, letra := range nombre {
+		fmt.Printf("Posición %d, letra \"%s\"\n", posicion, string(letra))
+	}
+
+	fmt.Printf("Hay %d letras en total\n", len(nombre))
 }
