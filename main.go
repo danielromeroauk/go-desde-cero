969c27238b0ce875d76ad7b384380f33637b3365
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 19:13:31 -0500

    Retorna error personalizado

--- main.go
+++ main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 func main() {
-	saludo, _ := saludar("Daniel", "Romero", 19)
+	saludo, _, err := saludar("Daniel", "R.", 19)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	fmt.Println("saludo:", saludo)
 }
 
-func saludar(nombre, apellido string, edad uint8) (texto string, booleano bool) {
+func saludar(nombre, apellido string, edad uint8) (texto string, booleano bool, err error) {
 	booleano = edad >= 18
 	texto = fmt.Sprintf("Hola %s %s", nombre, apellido)
+
+	if len(apellido) <= 2 {
+		err = fmt.Errorf("El apellido '%s' no es válido", apellido)
+	}
 
 	return
 }
