03a7061c7a7af3640f06346d210f819494715dc2
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 19:03:59 -0500

    Crea una función que retorna dos valores

--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import (
 )
 
 func main() {
-	saludo := saludar("Daniel", "Romero")
+	saludo, mayor := saludar("Daniel", "Romero", 19)
 
-	fmt.Println(saludo)
+	fmt.Println("saludo:", saludo)
+	fmt.Println("mayor:", mayor)
 }
 
-func saludar(nombre, apellido string) string {
-	return fmt.Sprintf("Hola %s %s", nombre, apellido)
+func saludar(nombre, apellido string, edad uint8) (string, bool) {
+	booleano := edad >= 18
+	texto := fmt.Sprintf("Hola %s %s", nombre, apellido)
+
+	return texto, booleano
 }
