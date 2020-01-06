ae06902b356ac511cd3bee607ee9ec250e244e73
Daniel Romero <danielromeroauk@gmail.com>
2020-01-05 19:37:59 -0500

    Compara la firma de la función con parámetros agrupados y separados

--- main.go
+++ main.go
@@ -8,19 +8,13 @@ import (
 func main() {
 	fmt.Println("saludo1:", reflect.TypeOf(saludo1))
 	fmt.Println("saludo2:", reflect.TypeOf(saludo2))
-
-	conNombre := saludo1("Daniel")
-	conApellido := saludo2("Guillermo")
-
-	fmt.Println(conNombre)
-	fmt.Println(conApellido)
 }
 
-func saludo1(nombre string) string {
+func saludo1(nombre string, apellido string) string {
 	return fmt.Sprintf("Hola %s", nombre)
 }
 
-func saludo2(nombre string) (texto string) {
+func saludo2(nombre, apellido string) (texto string) {
 	texto = fmt.Sprintf("Hola %s", nombre)
 	return
 }
